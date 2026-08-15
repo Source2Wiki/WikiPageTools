71a13abd419bef7ced008e34e66ad18e6cb82984
Angel <angelcazacu8@gmail.com>
2026-08-15 23:53:55 +0200

    bump version

--- WikiPageTools/Program.cs
+++ WikiPageTools/Program.cs
@@ -11,7 +11,7 @@ namespace FGDDumper
 {
     public static class EntityPageTools
     {
-        private const string Version = "1.3.4";
+        private const string Version = "1.3.5";
 
         public static string WikiRoot { get; private set; } = string.Empty;
 
